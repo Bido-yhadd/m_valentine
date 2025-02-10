a5fa7db7620c5389f62c227eb9812160018ae1a1
younes <y.haddouti@gmail.com>
2025-02-10 21:06:55 +0100

    change of website into two parts

--- password.js
+++ password.js
@@ -1,4 +1,15 @@
-// password.js
 window.config = {
-    correctPassword: btoa('michelle') // Encode the password with btoa
+    correctPassword: "bWljaGVsbGU=" // Example Base64 encoded password ("michelle")
 };
+
+function checkPassword() {
+    var password = document.getElementById("passwordInput").value;
+    var correctPassword = window.config.correctPassword;
+
+    if (btoa(password) === correctPassword) {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    } else {
+        document.getElementById("errorMessage").style.display = "block";
+    }
+}
